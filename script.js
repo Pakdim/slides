335e71a0cc74aa4470a41932e9fd78974f0b18d4
Pakdim <73749172+Pakdim@users.noreply.github.com>
2021-03-27 17:44:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const leftArrow = document.getElementsByClassName('arrow left')[0];
+const rightArrow = document.getElementsByClassName('arrow right')[0];
+
+const slides = document.getElementsByClassName('slide');
